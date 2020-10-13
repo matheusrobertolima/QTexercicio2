ab2710be2b8d0676a909ddc049c1fb6f4dc4f04b
Matheus <Matheus@DESKTOP-IN7N873>
2020-10-12 22:21:57 -0300

    Exemplo Final - Com erro

--- test/processador/ProcessadorTest.java
+++ test/processador/ProcessadorTest.java
@@ -11,11 +11,11 @@ public class ProcessadorTest {
 	@Test
 	public void testProcessador () {
 		
-		Fatura fatura = new Fatura ("998877665", "Matheus", 3300);
+		Fatura fatura = new Fatura ("998877665", "Matheus", 2500);
 		Boleto boleto1 = new Boleto ("123456789", "01/01/2020", 100.00);
 		Boleto boleto2 = new Boleto ("987654321", "02/01/2020", 200.00);
 		Boleto boleto3 = new Boleto ("135792468", "03/01/2020", 1000.00);
-		Boleto boleto4 = new Boleto ("873282984", "04/01/2020", 2000.00);
+		Boleto boleto4 = new Boleto ("873282984", "04/01/2020", 500.00);
 
 		
 		Processador processador = new Processador();
